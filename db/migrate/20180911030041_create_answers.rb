24385c3afaee90627f0de56b9b48bd65917b6153
Joanna Davidson <joanna@shrikesong.com>
2018-09-20 00:35:04 -0400

    Added procfile and adjusted a migration before deploying to Heroku

--- db/migrate/20180911030041_create_answers.rb
+++ db/migrate/20180911030041_create_answers.rb
@@ -2,6 +2,7 @@ class CreateAnswers < ActiveRecord::Migration[5.2]
   def change
     create_table :answers do |t|
       t.text :body
+      t.boolean :best_answer, default: false
       t.belongs_to :whiner, foreign_key: true
       t.belongs_to :whine, foreign_key: true
 
--- db/migrate/20180919172931_add_best_answer_to_answers.rb
+++
@@ -1,5 +0,0 @@
-class AddBestAnswerToAnswers < ActiveRecord::Migration[5.2]
-  def change
-    add_column :answers, :best_answer, :boolean, default: false
-  end
-end
